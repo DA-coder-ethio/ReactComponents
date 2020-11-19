c11431ea47fe78db77df483b277e350e0d2bc863
DA-coder-ethio <64494269+DA-coder-ethio@users.noreply.github.com>
2020-11-19 17:00:38 +0200

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Heading from "./Heading"; //file extention(.jsx)is optional
+import List from "./List";
+
+function App() {
+  return (
+    <div>
+      <Heading />
+      <List />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Heading.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+//react component function needs pascal case(starting letter capitaized)
+function Heading() {
+  return <h1>My Favourite Foods</h1>;
+}
+
+export default Heading;
